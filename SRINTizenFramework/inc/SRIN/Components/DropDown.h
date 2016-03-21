76f345c400f9a2ea11998433bdf29cdefc3ef236
Gilang M. Hamidy <g.hamidy@samsung.com>
2016-03-21 18:47:23 +0700

    DropDown components (Experimental commit)

---
+++ SRINTizenFramework/inc/SRIN/Components/DropDown.h
@@ -0,0 +1,76 @@
+/*
+ * DropDown.h
+ *
+ *  Created on: Mar 21, 2016
+ *      Author: Gilang M. Hamidy (g.hamidy@samsung.com)
+ */
+
+#ifndef SRIN_COMP_DROPDOWN_H_
+#define SRIN_COMP_DROPDOWN_H_
+
+
+
+#include "SRIN/Components/ComponentBase.h"
+#include "SRIN/Components/Adapter.h"
+
+namespace SRIN {
+namespace Components {
+
+class LIBAPI DropDown : public ComponentBase
+{
+private:
+	Evas_Object* parentComponent;
+	Evas_Object* dropdownComponent;
+	Adapter* dataSource;
+
+	ElementaryEvent dropdownButtonClick;
+	ElementaryEvent dropdownDismiss;
+	std::string text;
+	void* selectedItem;
+
+	void SetDataSource(Adapter* const & adapter);
+	Adapter*& GetDataSource();
+
+	void ShowDropdown();
+	void OnDropDownButtonClick(ElementaryEvent* viewSource, Evas_Object* objSource, void* eventData);
+
+protected:
+	virtual Evas_Object* CreateComponent(Evas_Object* root);
+
+public:
+	DropDown();
+
+	void ItemClick(Adapter::AdapterItem* item);
+
+	Event<DropDown*, void*> ItemSelected;
+	Property<DropDown, Adapter*, &DropDown::GetDataSource, &DropDown::SetDataSource> DataSource;
+
+};
+
+template<typename T>
+class DropDownAdapter : public AdapterItemClass<T>
+{
+public:
+	virtual std::string GetString(T* data, Evas_Object *obj, const char *part) final;
+	virtual Evas_Object* GetContent(T* data, Evas_Object *obj, const char *part) final;
+	virtual std::string GetText(T data) = 0;
+	virtual Evas_Object* GetIcon(T* data, Evas_Object root) = 0;
+	virtual ~DropDownAdapter() { };
+};
+
+}
+}
+
+template<typename T>
+inline std::string SRIN::Components::DropDownAdapter<T>::GetString(T* data, Evas_Object* obj, const char* part)
+{
+	return GetText(data);
+}
+
+template<typename T>
+inline Evas_Object* SRIN::Components::DropDownAdapter<T>::GetContent(T* data, Evas_Object* obj, const char* part)
+{
+	return GetIcon(data);
+}
+
+#endif /* DROPDOWN_H_ */
---
+++ SRINTizenFramework/src/Components/DropDown.cpp
@@ -0,0 +1,123 @@
+/*
+ * DropDown.cpp
+ *
+ *  Created on: Mar 21, 2016
+ *      Author: Gilang M. Hamidy (g.hamidy@samsung.com)
+ */
+
+#include "SRIN/Components/DropDown.h"
+
+#include <efl_extension.h>
+
+using namespace SRIN::Components;
+
+typedef struct {
+	DropDown* adapter;
+	Adapter::AdapterItem* item;
+} DropdownCallbackPackage;
+
+void DropdownAdapter_ClickCallback(void* data, Evas_Object* obj, void* eventInfo)
+{
+	DropdownCallbackPackage* package = static_cast<DropdownCallbackPackage*>(data);
+	package->adapter->ItemClick(package->item);
+}
+
+void DropdownAdapter_DeleteCallback(void* data, Evas_Object* obj, void* eventInfo)
+{
+	DropdownCallbackPackage* package = static_cast<DropdownCallbackPackage*>(data);
+	delete package;
+}
+
+LIBAPI Evas_Object* SRIN::Components::DropDown::CreateComponent(Evas_Object* root)
+{
+	Evas_Object* dropDown = elm_button_add(root);
+	elm_object_style_set(dropDown, "dropdown");
+
+	if(text.length())
+		elm_object_text_set(dropDown, text.c_str());
+
+	evas_object_smart_callback_add(dropDown, "clicked", SmartEventHandler, &dropdownButtonClick);
+	this->parentComponent = root;
+	return dropDown;
+}
+
+LIBAPI SRIN::Components::DropDown::DropDown() :
+	dropdownComponent(nullptr),
+	dataSource(nullptr),
+	parentComponent(nullptr),
+	DataSource(this),
+	selectedItem(nullptr)
+{
+	dropdownButtonClick += { this, &DropDown::OnDropDownButtonClick };
+}
+
+LIBAPI void SRIN::Components::DropDown::ShowDropdown()
+{
+	dropdownComponent = elm_ctxpopup_add(this->parentComponent);
+	elm_object_style_set(dropdownComponent, "dropdown/list");
+
+	// Generate drop down list
+	if(this->dataSource)
+	{
+		for(auto& item : this->dataSource->GetAll())
+		{
+			auto text = item.itemClass->GetString(item.data, dropdownComponent, "text");
+			auto icon = item.itemClass->GetContent(item.data, dropdownComponent, "icon");
+			auto package = new DropdownCallbackPackage({ this, &item });
+			item.objectItem = elm_ctxpopup_item_append(dropdownComponent, text.c_str(), icon, DropdownAdapter_ClickCallback, package);
+		}
+	}
+
+	elm_ctxpopup_direction_priority_set(dropdownComponent,
+			ELM_CTXPOPUP_DIRECTION_DOWN, ELM_CTXPOPUP_DIRECTION_UNKNOWN,
+			ELM_CTXPOPUP_DIRECTION_UNKNOWN, ELM_CTXPOPUP_DIRECTION_UNKNOWN);
+
+	//disable back pressed listener on app
+	//so back pressed listener on component can run
+	// Framework::AppInstance->EnableBackButtonCallback(false);
+	eext_object_event_callback_add(dropdownComponent, EEXT_CALLBACK_BACK, SmartEventHandler, &dropdownDismiss);
+	evas_object_smart_callback_add(dropdownComponent, "dismissed", SmartEventHandler, &dropdownDismiss);
+
+	//change position of the popup base on button
+	Evas_Coord x, y;
+	evas_pointer_canvas_xy_get(evas_object_evas_get(componentRoot), &x, &y);
+	evas_object_move(dropdownComponent, x, y);
+	evas_object_show(dropdownComponent);
+}
+
+LIBAPI void SRIN::Components::DropDown::OnDropDownButtonClick(ElementaryEvent* viewSource, Evas_Object* objSource,
+	void* eventData)
+{
+	ShowDropdown();
+}
+
+LIBAPI void SRIN::Components::DropDown::ItemClick(Adapter::AdapterItem* item)
+{
+	selectedItem = item->data;
+	text = item->itemClass->GetString(item->data, nullptr, "text");
+	elm_object_text_set(componentRoot, text.c_str());
+
+	if(dropdownComponent)
+	{
+		evas_object_del(dropdownComponent);
+		dropdownComponent = nullptr;
+	}
+
+	// Dereference the pointer to object item as now it is invalid
+	for(auto& item : this->dataSource->GetAll())
+	{
+		item.objectItem = nullptr;
+	}
+
+	ItemSelected(this, selectedItem);
+}
+
+LIBAPI void SRIN::Components::DropDown::SetDataSource(Adapter* const & adapter)
+{
+	this->dataSource = adapter;
+}
+
+LIBAPI Adapter*& SRIN::Components::DropDown::GetDataSource()
+{
+	return this->dataSource;
+}
