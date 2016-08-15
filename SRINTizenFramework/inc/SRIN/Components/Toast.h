b783cd9af1ead1b1809b7ec2f349556aeef0b239
Kevin Winata <k.winata@samsung.com>
2016-08-15 18:45:58 +0700

    Documenting Components/Toast

--- SRINTizenFramework/inc/SRIN/Components/Toast.h
+++ SRINTizenFramework/inc/SRIN/Components/Toast.h
@@ -11,6 +11,10 @@
 #include "SRIN/Framework/Application.h"
 #include "SRIN/Components/ComponentBase.h"
 
+/**
+ * Component that handles showing toast.
+ * It isn't inherited from ComponentBase, instead it provides static methods to show toast.
+ */
 namespace SRIN {
 	namespace Components {
 		class LIBAPI Toast : public EventClass {
@@ -20,9 +24,24 @@ namespace SRIN {
 			bool BackButtonPressed(EFL::EvasSmartEvent* event, Evas_Object* objSource, void* eventData);
 			EFL::EvasSmartEvent eventDismiss;
 		public:
+			/**
+			 * Constructor for Toast class.
+			 */
 			Toast();
 
+			/**
+			 * Method to show a toast with default timeout.
+			 *
+			 * @param message String that will be shown inside the toast.
+			 */
 			static void Show(const std::string& message);
+
+			/**
+			 * Method to show a toast with custom timeout.
+			 *
+			 * @param message String that will be shown inside the toast.
+			 * @param timeout How long the toast will be shown.
+			 */
 			static void Show(const std::string& message, double timeout);
 		};
 	}
