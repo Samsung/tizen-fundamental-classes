c8b51034c2754915cf17f06b47c9a23d5d779f60
Kevin Winata <k.winata@samsung.com>
2016-08-15 12:07:40 +0700

    Documenting Util/Sharing

--- SRINTizenFramework/inc/SRIN/Util/Sharing.h
+++ SRINTizenFramework/inc/SRIN/Util/Sharing.h
@@ -11,16 +11,33 @@
 #include <string>
 #include <efl_extension.h>
 
+/**
+ * Wrapper for Tizen App Control functions.
+ */
 namespace SRIN { namespace Util {
-
+	/**
+	 * Method to share a text by bluetooth, memo, etc.
+	 */
 	void ShareString(const std::string& str);
 
+	/**
+	 * Method to store text to clipboard.
+	 */
 	bool CopyStringToClipboard(const std::string& str, Evas_Object* source);
 
+	/**
+	 * Method to open native web browser and point it to a particular URL.
+	 */
 	void OpenURL(const std::string& url);
 
+	/**
+	 * Method to launch native multimedia viewer applications, e.g. video player.
+	 */
 	void LaunchViewer(const std::string& videoURL, const std::string& mimeType);
 
+	/**
+	 * Method to compose an email with one recipient.
+	 */
 	void ComposeMailForOne(const std::string& recipient, const std::string& subject, const std::string& text);
 
 }
