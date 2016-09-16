05e4273279de104f5e549c4ac8b58afee96caf31
Gilang M. Hamidy <g.hamidy@samsung.com>
2016-09-16 11:05:38 +0700

    Add Bundle template to easier bundling non-ObjectClass parameter type

---
+++ SRINTizenFramework/inc/SRIN/Framework/Bundle.h
@@ -0,0 +1,51 @@
+/*
+ * Bundle.h
+ *
+ *  Created on: Sep 16, 2016
+ *      Author: Gilang
+ */
+
+#ifndef BUNDLE_H_
+#define BUNDLE_H_
+
+#include "SRIN/Core.h"
+#include <stdexcept>
+#include <memory>
+
+namespace SRIN {
+namespace Framework {
+
+	template<class T>
+	class Bundle : public ObjectClass
+	{
+	private:
+		T value;
+		Bundle(T& value)
+		{
+			this->value = value;
+		}
+	public:
+
+
+		static Bundle<T>* Pack(T& value)
+		{
+			return new Bundle<T>(value);
+		}
+
+		static T Unpack(ObjectClass* data)
+		{
+			auto bundle = dynamic_cast<Bundle<T>*>(data);
+			if(bundle == nullptr)
+				throw std::runtime_error("ObjectClass is not a correct bundle instance");
+
+			std::shared_ptr<Bundle<T>> bundlePtr(bundle);
+			return bundlePtr->value;
+		}
+	};
+
+}
+}
+
+
+
+#endif /* BUNDLE_H_ */
