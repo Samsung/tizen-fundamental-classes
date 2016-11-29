9b12b88b9639f55d60f83be1ad6c79dd24d51d9b
Gilang Mentari Hamidy <g.hamidy@samsung.com>
2016-11-29 13:45:51 +0700

    Base Code for OAuthREST service classes

---
+++ TizenFundamentalClasses/inc/TFC/Net/OAuthREST.h
@@ -0,0 +1,25 @@
+/*
+ * OAuthREST.h
+ *
+ *  Created on: Nov 29, 2016
+ *      Author: gilang
+ */
+
+#ifndef TFC_NET_OAUTHREST_H_
+#define TFC_NET_OAUTHREST_H_
+
+#include "TFC/Net/REST.h"
+
+namespace TFC {
+namespace Net {
+
+	class OAuthRESTServiceBase
+	{
+
+	};
+
+}}
+
+
+
+#endif /* TFC_NET_OAUTHREST_H_ */
---
+++ TizenFundamentalClasses/src/Net/OAuthREST.cpp
@@ -0,0 +1,8 @@
+/*
+ * OAuthREST.cpp
+ *
+ *  Created on: Nov 29, 2016
+ *      Author: gilang
+ */
+
+#include "TFC/Net/OAuthREST.h"
