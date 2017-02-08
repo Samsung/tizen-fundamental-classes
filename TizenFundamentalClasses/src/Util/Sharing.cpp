6d987a74debb8d5dff8cc9a7bc2ab52ff2f875e4
vell <faidhon.nur@samsung.com>
2017-02-08 14:26:09 +0700

    Add ShareData functionality in Util

--- TizenFundamentalClasses/src/Util/Sharing.cpp
+++ TizenFundamentalClasses/src/Util/Sharing.cpp
@@ -10,6 +10,10 @@
 #include "TFC/Util/Sharing.h"
 #include "TFC/Core.h"
 #include <app_control.h>
+#include <media_content.h>
+#include <dlog.h>
+#include <sys/stat.h>
+#include <fstream>
 
 using namespace TFC::Util;
 
@@ -73,5 +77,53 @@ LIBAPI void TFC::Util::ComposeMailForOne(const std::string& recipient, const std
 	app_control_set_launch_mode(appControl, APP_CONTROL_LAUNCH_MODE_GROUP);
 
 	app_control_send_launch_request(appControl, NULL, NULL);
+	app_control_destroy(appControl);
+}
+
+LIBAPI void TFC::Util::ShareData(const std::string& text, const std::string& imagePathOri) {
+    dlog_print(DLOG_ERROR, LOG_TAG, "text = %s path = %s", text.c_str() , imagePathOri.c_str());
+
+    std::string imagePath(imagePathOri);
+    if(imagePath.empty()) {
+    	imagePath.append("/opt/usr/apps/com.srin.indramayu.tizen/res/image/Blank_Image_News_Revisi.png");
+    	dlog_print(DLOG_ERROR, LOG_TAG, "text 2 = %s path 2 = %s", text.c_str() , imagePath.c_str());
+    }
+
+	//get image filename
+	std::size_t found = imagePath.find_last_of("/\\");
+	std::string filename = "/opt/usr/media/Images/ggi/";
+	filename.append(imagePath.substr(found+1));
+
+	media_info_h mediaInfo = NULL;
+    mkdir("/opt/usr/media/Images/ggi/", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    std::ifstream  src(imagePath, std::ios::binary);
+    std::ofstream  dst(filename,   std::ios::binary);
+    dst << src.rdbuf();
+    dlog_print(DLOG_ERROR, LOG_TAG, "filename = %s", filename.c_str());
+
+    media_content_connect();
+    int ret = media_info_insert_to_db(filename.c_str(), &mediaInfo);
+    if ((MEDIA_CONTENT_ERROR_NONE == ret) && (NULL != mediaInfo))
+        dlog_print(DLOG_ERROR, LOG_TAG, "Insertion succeed, ret: %d", ret);
+    else {
+        dlog_print(DLOG_ERROR, LOG_TAG, "Insertion failed, ret: %d", ret);
+    }
+
+	app_control_h appControl;
+
+	app_control_create(&appControl);
+
+	app_control_set_operation(appControl, APP_CONTROL_OPERATION_SHARE);
+	app_control_set_uri(appControl, filename.c_str());
+	app_control_set_mime(appControl, "image/*");
+	app_control_add_extra_data(appControl, APP_CONTROL_DATA_PATH, filename.c_str());
+//	app_control_add_extra_data(appControl, APP_CONTROL_DATA_SUBJECT, "TEST SUBJECT");
+//	app_control_add_extra_data(appControl, APP_CONTROL_DATA_TITLE, "TEST TITLE");
+	app_control_add_extra_data(appControl, APP_CONTROL_DATA_TEXT, text.c_str());
+	//app_control_add_extra_data(appControl, APP_CONTROL_DATA_TEXT, str.c_str());
+	app_control_set_launch_mode(appControl, APP_CONTROL_LAUNCH_MODE_GROUP);
+
+	app_control_send_launch_request(appControl, NULL, NULL);
+
 	app_control_destroy(appControl);
 }
