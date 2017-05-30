29cf69174650734108ab2ddf53561b7b4f26654d
Gilang Mentari Hamidy <gilang.hamidy@gmail.com>
2017-05-30 10:50:02 +0700

    [FIX] Missing check for nullptr in destructor
    Destructor must check nullptr value on instance field Adapter prior
    dereferencing it to its member.

--- TizenFundamentalClasses/src/Components/GenericList.cpp
+++ TizenFundamentalClasses/src/Components/GenericList.cpp
@@ -187,8 +187,11 @@ TFC::Components::GenericList::~GenericList()
 	evas_object_smart_callback_del(genlist, "unrealized", EFL::EvasSmartEventHandler);
 	*/
 
-	this->dataSource->eventItemAdd -= EventHandler(GenericList::OnItemAdd);
-	this->dataSource->eventItemRemove -= EventHandler(GenericList::OnItemRemove);
+	if(dataSource)
+	{
+		this->dataSource->eventItemAdd -= EventHandler(GenericList::OnItemAdd);
+		this->dataSource->eventItemRemove -= EventHandler(GenericList::OnItemRemove);
+	}
 }
 
 LIBAPI void TFC::Components::GenericList::ResetScroll(bool animated)
